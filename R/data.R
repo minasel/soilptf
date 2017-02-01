2dfcdafe91d5a1faedc19251e047864beeaee276
Dewey Dunnington <dewey@fishandwhistle.net>
2017-02-01 15:16:09 -0400

    add average values

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Average values for soil texture classes based on the ROSETTA model
+#'
+#' These values are average values for theta_r, theta_s, log_alpha, log_n, Ks, K0, L
+#' (including uncertainty) by soil textural class. They are provided
+#'
+#' @format A data frame with 8735 rows and  19 variables. There are many columns,
+#'   only several of which are used within this package.
+#' \describe{
+#'   \item{TextureClass}{the name of the texture class (one of Clay, C loam, Loam, L Sand, Sand,
+#'   S C L, S Clay, Si C L, Si Clay, Si Loam, Silt, S loam)}
+#'   \item{N}{the number of observations on which the parameters are based}
+#'   \item{variable}{the variable of interest (one of theta_r, theta_s, log_alpha, log_n, Ks, K0, L)}
+#'   \item{value}{the value of the parameter}
+#'   \item{stdev}{the uncertainty associated with the value measurement (one standard deviation)}
+#'   \item{TC ID}{IDs that may be used outside of EC}
+#' }
+#'
+#' @references
+#' Schaap, M. G., Leij, F. J., & Van Genuchten, M. T. (2001). ROSETTA: a computer program
+#' for estimating soil hydraulic parameters with hierarchical pedotransfer functions.
+#' Journal of Hydrology, 251(3), 163–176. \url{https://doi.org/10.1016/S0022-1694(01)00466-8}
+
+#'
+#' @source \url{https://www.cals.arizona.edu/research/rosetta/download/rosetta.pdf}
+"rosettaSoilClass"
+
+# load within package so the data can be used in getClimateSites()
+data("rosettaSoilClass", envir=environment())
---
+++ data-raw/makeRosettaSoilTextures.R
@@ -0,0 +1,24 @@
+
+avs <- read.delim('data-raw/rosettaav.txt', colClasses="character", header=F)[[1]]
+avs <- matrix(avs, byrow = TRUE, ncol = 16)
+avs <- as.data.frame(avs)
+vals <- avs[c(1, 2, 3, 5, 7, 9, 11, 13, 15)]
+errs <- avs[c(1, 2, 4, 6, 8, 10, 12, 16, 16)]
+labels <- c("TextureClass", "N", "theta_r", "theta_s", "log_alpha", "log_n", "Ks", "K0", "L")
+names(vals) <- labels
+names(errs) <- labels
+
+vals$N <- as.integer(as.character(vals$N))
+errs$N <- as.integer(as.character(errs$N))
+for(name in labels[3:length(labels)]) {
+  vals[[name]] <- as.numeric(as.character(vals[[name]]))
+  errs[[name]] <- as.numeric(gsub(x=as.character(errs[[name]]), pattern="[()]", replacement = ""))
+}
+
+rosettaSoilClass <- cbind(reshape2::melt(vals, id.vars=c("TextureClass", "N")),
+                          reshape2::melt(errs, id.vars=c("TextureClass", "N"),
+                                         value.name="stdev")[-1:-3])
+
+rosettaSoilClass <- rosettaSoilClass[order(rosettaSoilClass$TextureClass, rosettaSoilClass$variable),]
+
+devtools::use_data(rosettaSoilClass)
